9b7e6ee65cc967c9d23e082da90c7cc75e09194c
Adam Michaud <invariel@gmail.com>
2017-02-28 00:16:56 -0500

    Let's be more and less exclamatory!
    !¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡!¡

--- javascript-source/lang/english/systems/systems-raidSystem.js
+++ javascript-source/lang/english/systems/systems-raidSystem.js
@@ -1,4 +1,4 @@
-$.lang.register('raidsystem.raid', 'We are raiding http://twitch.tv/$1 . Get in their chat and wait for the raid call:  TransientGamers Raid!');
+$.lang.register('raidsystem.raid', 'We are raiding http://twitch.tv/$1! Get in their chat and wait for the raid call:  TransientGamers RAID');
 $.lang.register('raidsystem.raid.usage', 'Usage: !raid [username]');
 $.lang.register('raidsystem.raided', 'We are being raided by $1! This is the $2 time $1 has raided. Go check out their channel and hit that follow button at http://twitch.tv/$1 !');
 $.lang.register('raidsystem.raider.usage', 'Usage: !raider [username] -- !raider count [username]');
