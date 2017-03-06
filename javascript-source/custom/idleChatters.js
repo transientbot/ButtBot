be4f6b0fc8f0a2152ddb0cff5ff17a5b9caf15dc
Adam Michaud <invariel@gmail.com>
2017-03-05 20:01:59 -0500

    Module for checking idlers.
    Added a couple of commands to check that idlers are idle, and stuff.

--- javascript-source/custom/idleChatters.js
+++ javascript-source/custom/idleChatters.js
@@ -81,12 +81,99 @@
 		return active && randomTime;
 	}
 
+	function convertMilliseconds (duration)
+	{
+		var ret = "";
+		var colons = [ ":", ":", ":", "." ];
+
+		[3600000, 60000, 1000, 1].forEach (element, index, array)
+		{
+			var val = Math.floor (duration / element);
+
+			if (val > 0 && ret != "")
+			{
+				ret += (val < 10 ? "0" : "") + val + colons[index] + " ";
+			}
+			duration -= val * element;
+		}
+		return ret + " to idle";
+	}
+
+    $.bind('command', function(event) {
+        var sender = event.getSender().toLowerCase(),
+            command = event.getCommand(),
+            args = event.getArgs(),
+            random;
+
+        if (command.equalsIgnoreCase('reportidletimes'))
+        {
+        	if (!args[0])
+        	{
+        		if (active)
+        		{
+        			$.say ($.whisperPrefix(sender, true) + "Points idle duration: " + $.idlePointsDuration () + " and random idle duration: " + $.idleRandomDuration());
+        		}
+        		else
+        		{
+        			$.say ($.whisperPrefix(sender, true) + "Idle timers are not active.");
+        		}
+        	}
+        }
+        else if (command.equalsIgnoreCase('reportactiveusers'))
+        {
+        	if (!args[0] || (args[0] != "points" && args[0] != "random"))
+        	{
+        		$.say ($.whisperPrefix(sender, true) + "Usage:  !reportactiveusers [points | random]");
+        	}
+        	else
+        	{
+        		var duration;
+        		if (args[0] == "points")
+        		{
+        			duration = $.idlePointsDuration();
+        		}
+        		else if (args[0] == "random")
+        		{
+        			duration = $.idleRandomDuration();
+        		}
+
+        		var ret = "";
+        		var retval = $.getRecentChatters(duration);
+        		var now = new Date().getTime();
+
+        		retval.forEach(function (element, index, array)
+        		{
+        			ret += "" + element + " [" + convertMilliseconds (recentChatters[element][0] + duration - now) + "] ";
+        		});
+
+        		if (retval.length == 0)
+        		{
+        			$.say ($.whisperPrefix (sender, true) + "Nobody is active at the moment.");
+        		}
+        		else if (retval.length == 1)
+        		{
+        			$.say ($.whisperPrefix (sender, true) + "The only active person is " + ret);
+        		}
+        		else
+        		{
+        			$.say ($.whisperPrefix (sender, true) + "The following people are active: " + ret);
+        		}
+        	}
+        }
+    });
+
 	$.bind('ircChannelMessage', function (event)
 	{
 		if (active)
 		{
 			receivedChatMessage (event.getSender().toLowerCase());
 		}
+	});
+
+	$.bind('initready', function (event)
+	{
+		$.registerChatCommand('./custom/idlechatters.js', 'reportidletimes', 2);
+		$.registerChatCommand('./custom/idlechatters.js', 'reportactiveusers', 2);
 	});
 
     $.reloadIdle = idleUpdate;
