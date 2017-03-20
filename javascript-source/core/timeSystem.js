255ac5a55faba48fd1e405e0b112c12f71a61a76
Adam Michaud <invariel@gmail.com>
2017-03-20 17:23:02 -0400

    Discord is weird.
    Etc.

--- javascript-source/core/timeSystem.js
+++ javascript-source/core/timeSystem.js
@@ -377,7 +377,7 @@
                 if (!$.isMod(username) && !$.isAdmin(username) && !$.isSub(username) && $.inidb.exists('time', username) && Math.floor(parseInt($.inidb.get('time', username)) / 3600) >= hoursForLevelUp &&  parseInt($.getUserGroupId(username)) > regularsGroupId) {
                     if (!$.hasModList(username)) { // Added a second check here to be 100% sure the user is not a mod.
                         $.setUserGroupById(username, regularsGroupId);
-						$.discord.say("mod-chat", "@TransientGamers#4061: " + username + " has finally reached regular status!");
+						$.discord.say("mod-chat", $.discord.userPrefix("@TransientGamers#4061") + ": " + username + " has finally reached regular status!");
                         if (timeLevelWarning) {
                             $.say($.lang.get(
                                 'timesystem.autolevel.promoted',
