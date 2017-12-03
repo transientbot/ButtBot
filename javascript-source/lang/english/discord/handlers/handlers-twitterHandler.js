49a9c2ab3ee3a5d2fc48593a63b2631fd33075a6
Adam Michaud <invariel@gmail.com>
2017-12-03 02:56:43 -0500

    Erk, delete everything and start over.

--- javascript-source/lang/english/discord/handlers/handlers-twitterHandler.js
+++
@@ -1,4 +0,0 @@
-$.lang.register('discord.twitterhandler.usage', 'Usage: !twitterhandler [toggle / channel]');
-$.lang.register('discord.twitterhandler.toggle', 'Twitter annoucements have been $1.');
-$.lang.register('discord.twitterhandler.channel.usage', 'Usage: !twitterhandler channel [channel name]');
-$.lang.register('discord.twitterhandler.channel.set', 'Twitter annoucements will now be made in channel #$1');
