7b5f1bb8dc909fdc0a5845fd7edc6536352827da
Adam Michaud <invariel@gmail.com>
2017-12-04 01:22:00 -0500

    Correcting the format of the array.
    Because this shouldn't break.

--- javascript-source/custom/a_definedRanks.js
+++ javascript-source/custom/a_definedRanks.js
@@ -4,9 +4,8 @@
 {
 	function loadIndividualRanks ()
 	{
-		// Format:  [ "user1" : [ "rank1", "rank2", "rank3", ...], ];
-		return [
-		];
+		// Format:  { "user1" : [ "rank1", "rank2", "rank3", ...], };
+		return { };
 	};
 
 	$.loadIndividualRanks = loadIndividualRanks;
