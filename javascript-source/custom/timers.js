368380d8356fe59005772fdff1bf7eca0ff89328
Adam Michaud <invariel@gmail.com>
2018-05-01 00:12:53 -0400

    Adding "addtimer" command.
    Usage:  !addtimer "Text to display in chat" #minutes
    
    Supposed to say the message in chat after minutes elapsed.

---
+++ javascript-source/custom/timers.js
@@ -0,0 +1,24 @@
+(function() {
+	$.bind('command', function(event) {
+	var sender = event.getSender(),
+		command = event.getCommand(),
+		args = event.getArgs(),
+		random;
+	
+	if (command.equalsIgnoreCase('addtimer'))
+	{
+		if (args.length < 2)
+		{
+			$.say("Usage: !timer \"Reminder.\" <number of minutes>");
+		}
+		else
+		{
+			setTimeout(function () { $.say (args[0]); }, args[1] * 60 * 1000);
+		}
+	}
+	
+	$.bind('initReady', function (event)
+	{
+		$.registerChatCommand('./custom/timers.js', 'addtimer', 2);
+	});
+})();
